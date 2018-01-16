f5d3253966b16080bd5f7b62cf5282a06a7730ca
Eric Neidhardt <eric.neidhardt@gmail.com>
2018-01-16 19:57:00 +0100

    wip

--- relay.go
+++ relay.go
@@ -1,1 +1,12 @@
 package main
+
+func relayStreamToWSClients(stream <-chan *[]byte, clients <-chan *wsClient) {
+	go func() {
+		newClient := <-clients
+		go relayStreamToWS(stream, newClient)
+	}()
+}
+
+func relayStreamToWS(stream <-chan *[]byte, client *wsClient) {
+
+}
