983ac76772486cdf1ca578c7f22d0d1ede2f9d0d
Eric Neidhardt <eric.neidhardt@gmail.com>
2019-09-06 10:08:13 +0200

    Refactor

--- stream_test.go
+++ stream_test.go
@@ -7,9 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWaitForStream(t *testing.T) {
-	// arrange
-	streamReceiver := waitForStream(":8989", "test")
+func startSendingSampleStream() error {
 	streamSender := exec.Command("ffmpeg",
 		"-i", "testdata/sample.mp4",
 		"-f", "mpegts",
@@ -21,15 +19,23 @@ func TestWaitForStream(t *testing.T) {
 		"-q:v", "6",
 		"http://localhost:8989/test")
 
+	_, err := streamSender.Output()
+	return err
+}
+
+func TestWaitForStream(t *testing.T) {
+	// arrange
+	stream := waitForStream(":8989", "test")
+
 	go func() {
-		_, err := streamSender.Output()
+		err := startSendingSampleStream()
 		if err != nil {
 			assert.Fail(t, "Error while sending stream")
 		}
 	}()
 
 	// action
-	firstChunk := <-streamReceiver
+	firstChunk := <-stream
 
 	// verify
 	assert.NotEmpty(t, firstChunk)
