b47d69fa4675af1cdbc137a1fae452262e248cc0
Eric Neidhardt <eric.neidhardt@gmail.com>
2019-09-06 10:01:34 +0200

    Added test to check if stream can be received

--- stream_test.go
+++ stream_test.go
@@ -1,11 +1,36 @@
 package main
 
-import "testing"
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestWaitForStream(t *testing.T) {
 	// arrange
-	var stream = waitForStream(":8989", "test")
+	streamReceiver := waitForStream(":8989", "test")
+	streamSender := exec.Command("ffmpeg",
+		"-i", "testdata/sample.mp4",
+		"-f", "mpegts",
+		"-codec:v", "mpeg1video",
+		"-s", "1280x720",
+		"-rtbufsize", "2048M",
+		"-r", "30",
+		"-b:v", "3000k",
+		"-q:v", "6",
+		"http://localhost:8989/test")
+
+	go func() {
+		_, err := streamSender.Output()
+		if err != nil {
+			assert.Fail(t, "Error while sending stream")
+		}
+	}()
 
 	// action
-	<-stream
+	firstChunk := <-streamReceiver
+
+	// verify
+	assert.NotEmpty(t, firstChunk)
 }
