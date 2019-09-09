bde76ce306a30e1454f40318713697388158d604
EricNeid <eric.neidhardt@gmail.com>
2019-09-09 15:49:21 +0200

    Moved server mux creation out of goroutine

--- stream.go
+++ stream.go
@@ -11,42 +11,42 @@ func waitForStream(port string, secret string, done <-chan bool) <-chan *[]byte 
 	log.Printf("Listening for incoming stream on %s/%s\n", port, secret)
 
 	stream := make(chan *[]byte)
-	go func() {
-		streamReader := http.NewServeMux()
-		streamReader.HandleFunc("/"+secret, func(w http.ResponseWriter, r *http.Request) {
-			log.Println("Stream client connected: " + r.RemoteAddr)
+	streamReader := http.NewServeMux()
+	streamReader.HandleFunc("/"+secret, func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Stream client connected: " + r.RemoteAddr)
 
-			input := r.Body
-			defer input.Close()
+		input := r.Body
+		defer input.Close()
 
-			// read from input until EOF
-			buffer := make([]byte, bufferSize)
-			for {
-				var readCount int
-				var err error
+		// read from input until EOF
+		buffer := make([]byte, bufferSize)
+		for {
+			var readCount int
+			var err error
 
-				select {
-				case <-done:
-					log.Println("Stop waiting for input stream")
-					return
-				default:
-					readCount, err = input.Read(buffer[:cap(buffer)])
-				}
-
-				if readCount > 0 {
-					chunk := buffer[:readCount]
-					stream <- &chunk
-				}
-
-				if err == io.EOF {
-					log.Println("Stream EOF reached")
-					break
-				} else {
-					log.Printf("Error while reading from stream: %s\n", err.Error())
-					break
-				}
+			select {
+			case <-done:
+				log.Println("Stop waiting for input stream")
+				return
+			default:
+				readCount, err = input.Read(buffer[:cap(buffer)])
 			}
-		})
+
+			if readCount > 0 {
+				chunk := buffer[:readCount]
+				stream <- &chunk
+			}
+
+			if err == io.EOF {
+				log.Println("Stream EOF reached")
+				break
+			} else {
+				log.Printf("Error while reading from stream: %s\n", err.Error())
+				break
+			}
+		}
+	})
+	go func() {
 		http.ListenAndServe(port, streamReader)
 	}()
 	return stream
