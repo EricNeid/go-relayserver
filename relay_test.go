75213f4c8700a0e0bd9ccff642111530962ddd60
EricNeid <eric.neidhardt@gmail.com>
2019-09-09 15:48:55 +0200

    Fixed blocking test

--- relay_test.go
+++ relay_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRelayStreamToWSClients(t *testing.T) {
 	// arrange
-	done := make(chan bool)
+	done := make(chan bool, 1)
 	stream := waitForStream(":8993", "test", done)
 	clients := waitForWSClients(":8994")
 	con, err := connectClient(":8994")
