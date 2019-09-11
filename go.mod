02c98aa6f6eb1fc5538a0fe351b90e3f7f9096ac
EricNeid <eric.neidhardt@gmail.com>
2019-09-11 10:34:58 +0200

    Wip new stream receiver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/EricNeid/go-relayserver
 
 require github.com/gorilla/websocket v1.4.1
+
+go 1.13
--- stream.go
+++ stream.go
@@ -1,12 +1,86 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
+
+const timeFormat string = "Mon Jan 2 15:04:05 2006"
+
+type streamServer struct {
+	router            *http.ServeMux
+	isStreamConnected chan bool
+	inputStream       chan *[]byte
+	done              <-chan bool
+}
+
+func newStreamServer(port string) *streamServer {
+	router := http.NewServeMux()
+
+	return &streamServer{
+		router:            router,
+		isStreamConnected: make(chan bool, 1),
+		inputStream:       make(chan *[]byte),
+	}
+}
+
+func (s *streamServer) routes(secret string) {
+	s.router.HandleFunc("/"+secret, s.log(s.handleStream))
+}
+
+func (s *streamServer) handleStream(w http.ResponseWriter, r *http.Request) {
+	s.isStreamConnected <- true
+
+	input := r.Body
+	defer input.Close()
+
+	buffer := make([]byte, bufferSize)
+	for {
+		var readCount int
+		var err error
+
+		isDone := false
+
+		select {
+		case <-s.done:
+			isDone = true
+		default:
+			readCount, err = input.Read(buffer[:cap(buffer)])
+		}
+
+		if isDone {
+			log.Println("Stop waiting for input stream")
+			break
+		}
+
+		if readCount > 0 {
+			chunk := buffer[:readCount]
+			s.inputStream <- &chunk
+		}
+
+		if err == io.EOF {
+			log.Println("Stream EOF reached")
+			break
+		} else if err != nil {
+			log.Printf("Error while reading from stream: %s\n", err.Error())
+			break
+		}
+	}
+	<-s.isStreamConnected
+}
+
+func (s *streamServer) log(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		timestamp := time.Now()
+		fmt.Printf("%s - streamServer - %s\n", timestamp.Format(timeFormat), r.URL.Path)
+		fn(w, r)
+	}
+}
 
 func waitForStream(port string, secret string, done <-chan bool) (*http.Server, <-chan *[]byte) {
 	log.Printf("Listening for incoming stream on %s/%s\n", port, secret)
