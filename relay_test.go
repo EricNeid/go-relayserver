7d3bd33e99fc14eb13427e5f259efaf9dc7cfe90
EricNeid <eric.neidhardt@gmail.com>
2019-09-18 14:54:47 +0200

    Finished refactor

--- relay_test.go
+++ relay_test.go
@@ -1,12 +1,26 @@
 package main
 
-/*
+import (
+	"testing"
+	"time"
+)
+
 func TestRelayStreamToWSClients(t *testing.T) {
 	// arrange
-	done := make(chan bool, 1)
-	_, stream := waitForStream(":8993", "test", done)
-	clients := waitForWSClients(":8994")
-	con, err := connectClient(":8994")
+	streamServer := newStreamServer(":8080", "test")
+	streamServer.routes()
+
+	webSocketServer := newWebSocketServer(":8081")
+	webSocketServer.routes()
+
+	go func() {
+		streamServer.listenAndServe()
+	}()
+	go func() {
+		webSocketServer.listenAndServe()
+	}()
+
+	con, err := connectClient(":8081")
 	ok(t, err)
 	defer con.Close()
 	go func() {
@@ -18,17 +32,16 @@ func TestRelayStreamToWSClients(t *testing.T) {
 		}
 	}()
 
-	relayStreamToWSClients(stream, clients)
-
 	// action
+	relayStreamToWSClients(streamServer.inputStream, webSocketServer.incomingClients)
 	start := time.Now()
-	err = sendData(":8993", "test", "Hallo, Welt")
+	err = sendData(":8080", "test", "Hallo, Welt")
 	timeTrack(t, start, "TestRelayStreamToWSClients: Sending data")
 
 	// verify
 	ok(t, err)
 
-	//cleanup
-	done <- true
+	// cleanup
+	streamServer.shutdown()
+	webSocketServer.shutdown()
 }
-*/
